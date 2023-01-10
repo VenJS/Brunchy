fee9e039c78f0e4651e6f89e1c524240620210a3
vkiskinov <vkiskinov@gmail.com>
2023-01-10 10:28:33 +0200

    feat: header component created

---
+++ brunchy/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.header}>Header</h1>
+        <div className={styles.wrapper}>
+            <a href='https://www.dreamshot.bg/' target="__blank"
+                rel="noopener noreferrer" className={styles.link}>About</a>
+            <a href='https://www.dreamshot.bg/' target="__blank"
+                rel="noopener noreferrer" className={styles.link}>Call us</a>
+            
+            <div className={styles['free_delivery_logo']}>
+                <span className={styles['free_delivery']}>Free Delivery</span>
+                <img src='/delivery.png' alt='' className={styles.icon}></img>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
