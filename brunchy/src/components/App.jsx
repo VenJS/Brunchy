15a8a5550ea594154d0435553f1a9cccf4d6d490
vkiskinov <vkiskinov@gmail.com>
2023-01-10 12:17:30 +0200

    feat: put toghether all components

---
+++ brunchy/src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header/Header";
+import Heading from "./Heading/Heading";
+import Card from "./Card/Card";
+import Footer from "./Footer/Footer";
+import styles from './App.module.scss'
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchedData = async () => {
+      const result = await fetch("./data.json")
+        .then((res) => res.json())
+        .then((res) => setData(res.items));
+    };
+    fetchedData();
+  }, []);
+
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      <Header></Header>
+      <Heading></Heading>
+      <div className={styles['card-container']}>
+      {data.map((arr) => {
+        return (
+          <Card
+            name={arr.name}
+            description={arr.description}
+            price={arr.price}
+            image={arr.image}
+          ></Card>
+        );
+      })}
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
--- brunchy/src/components/Card/Card.jsx
+++ brunchy/src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from './Card.module.scss'
 
-const Card = () => {
+const Card = ({name, description, price, image}) => {
   return (
     <div className={styles.container}>
-        <img src='/1.png' alt='' className={styles['food-image']}></img>
+        <img src={image} alt='' className={styles['food-image']}></img>
         <div className={styles.wrapper}>
-            <h3 className={styles['food-title']}>Butter Pancakes</h3>
-            <p className={styles.description}>With honey</p>
+            <h3 className={styles['food-title']}>{name}</h3>
+            <p className={styles.description}>{description}</p>
             <div className={styles['price-wrapper']}>
                 <span className={styles['dollar-sign']}>$</span>
-                <span className={styles.price}>14.50</span>
+                <span className={styles.price}>{price}</span>
             </div>
         </div>
         <img src='/basket.png' alt='' className={styles.basket}></img>
